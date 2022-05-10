cd508ec16dd1afed783199511fe3fc3ed4913ed8
money <374648769@qq.com>
2022-05-10 21:18:55 +0800

    更新朋友圈、彩虹屁接口

--- src/component/RainbowFartService.js
+++ src/component/RainbowFartService.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 
 let cache = "对不起，服务请求失败!";
-const URL = "https://chp.shadiao.app/api.php";
+const URL = "https://api.shadiao.app/chp";
 
 function RainbowFart() {
 }
@@ -11,7 +11,7 @@ RainbowFart.prototype = {
     constructor: RainbowFart,
     doService: async function (args) {
         return await axios.get(URL).then(res => {
-            let txt = res.data || cache;
+            let txt = res.data?.data.text || cache;
             if (txt !== "对不起，服务请求失败!")
                 cache = txt;
             return cache;
